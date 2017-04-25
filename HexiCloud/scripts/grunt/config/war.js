d07945da631bb0f31c1670f13dc7a288604e58f6
Mohan Chandra <mohan.chandra@oracle.com>
2017-04-25 18:52:19 +0530

    Adding configurations for dev deployment

--- HexiCloud/scripts/grunt/config/war.js
+++ HexiCloud/scripts/grunt/config/war.js
@@ -7,7 +7,8 @@ module.exports =  {
         target: {
           options: {
             war_dist_folder: 'dist',      /* Folder to generate the WAR into */
-            war_name: 'hexiCloud'            /* The name fo the WAR file (.war will be the extension) */
+//            war_name: 'hexiCloud'            /* War name for prod and test */
+           war_name: 'hexiCloudDev'            /* War name for Dev */
 //            webxml_webapp_version: '2.5', /* I needed this older version for JCS-SX */  
 //            war_extras: [ {filename: 'grunt-war-credits.txt', data: 'This line will appear in the file!\n see http://likeahouseafire.com/2016/04/22/using-grunt-to-create-war/ '}, 
 //                          {filename: 'WEB-INF/weblogic.xml', data: '<?xml version="1.0" encoding="UTF-8"?>\n<weblogic-web-app xmlns="http://www.bea.com/ns/weblogic/90" xmlns:j2ee="http://java.sun.com/xml/ns/j2ee" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.bea.com/ns/weblogic/90 http://www.bea.com/ns/weblogic/90/weblogic-web-app.xsd">\n  <jsp-descriptor>\n    <keepgenerated>true</keepgenerated>\n    <debug>true</debug>\n  </jsp-descriptor>\n  <context-root>/yojet</context-root>\n</weblogic-web-app>'}],
