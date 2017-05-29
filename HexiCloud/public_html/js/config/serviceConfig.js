8d849e2ce569c593e1f0f05ea517011d2d38fcfa
Nagaraju Yasarapu <nagaraju.yasarapu@oracle.com>
2017-05-29 18:18:43 +0530

    Modified Use Case stylings in dashboard..

--- HexiCloud/public_html/js/config/serviceConfig.js
+++ HexiCloud/public_html/js/config/serviceConfig.js
@@ -188,7 +188,8 @@ define(['knockout', 'jquery', 'config/sessionInfo', 'ojs/ojrouter'
 
         self.getUserClmData = function (userId) {
             var defer = $.Deferred();
-            var serverURL = self.portalRestHost() + "/services/rest/getClmData/" + userId + "/";
+            var serverURL = "js/pages/servicesMini/servicesMini.json";
+//            var serverURL = self.portalRestHost() + "/services/rest/getClmData/" + userId + "/";
             $.ajax({
                 type: "GET",
                 url: serverURL,
